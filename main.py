39f480d5c9a4615bfc241741fa929dd72d9ed0fb
Aman-am <amanmehta015@gmail.com>
2019-05-21 22:04:54 +0530

    Update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+import json, datetime, decimal
+from sqlalchemy import func
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:qwerty*@localhost/apitest'
@@ -28,6 +30,24 @@ class Places(db.Model):
         self.place_name = place_name
         self.admin_name1 = admin_name1
 
+    def __repr__(self):
+        """"""
+        return "<Place - '%s': '%s' - '%s'>" % (self.key, self.place_name, self.admin_name1)
+
+
+class PlacesSchema(ma.ModelSchema):
+    class Meta:
+        model = Places
+
+
+def alchemyencoder(obj):
+    """JSON encoder function for SQLAlchemy special classes."""
+    if isinstance(obj, datetime.date):
+        return obj.isoformat()
+    elif isinstance(obj, decimal.Decimal):
+        return float(obj)
+
+
 # API to create new location
 @app.route("/post_location", methods=["POST"])
 def add_location():
@@ -51,19 +71,36 @@ def add_location():
     db.session.add(new_location)
     db.session.commit()
 
-    return jsonify({'key':key,'place_name':place_name,'city':city, 'address':'added'})
+    new_framework = db.session.query(Places).filter_by(key =new_location.key, place_name = new_location.place_name, admin_name1 = new_location.admin_name1 ).all()
+    places_schema = PlacesSchema(many= True)
+    #print schema
+    output = places_schema.dump(new_framework).data
+    output = json.dumps({'Place': output}, default=alchemyencoder)
+    j = output.replace('"[', '[').replace(']"', ']')
+
+    return (json.dumps(json.loads(j), indent=2))
+
 
 # API to fetch nearby points
 @app.route("/get_using_postgres", methods=["GET"])
 def get_using_postgres():
-    latitude = request.json['lat']
-    longitude = request.json['long']
-    radius = request.json['radius']
+    try:
+        lat = request.json['lat']
+        long = request.json['long']
+        radius = request.json['radius']
+    except:
+        return "Please send Lat, Long and Radius"
 
-    # SELECT events.id, events.name FROM events WHERE earth_box( {current_user_lat}, {current_user_lng}, {radius_in_metres}) @> ll_to_earth(events.lat, events.lng);
-    points = db.session.query(places.latitude, Places.longitude).filter_by(earth_box( latitude, longitude, radius) @> ll_to_earth(Places.latitude, Places.longitude))
-    return json.dumps(points)
+    # SELECT * FROM public.places WHERE earth_box( ll_to_earth(106.332, 106.331), 5000) @> ll_to_earth(places.latitude, places.longitude);
+    loc_given = func.earth_box(func.ll_to_earth(lat, long ), radius)
+    loc_test = func.ll_to_earth(Places.latitude, Places.longitude)
+    result = Places.query.filter(loc_given.op("@>")(loc_test))
+    place_schema = PlacesSchema(many=True)
+    output = place_schema.dump(result).data
+    output = json.dumps({'place': output}, default=alchemyencoder)
+    # j = output.replace('"[', '[').replace(']"', ']')
 
+    return (json.dumps(json.loads(output), indent=2))
 
 @app.route("/")
 def home():
